8932069a9c54f5a7d0fa5f28d5aba0071cae7ebc
smoofy24 <splavecl@gmail.com>
2024-06-10 20:42:30 +0200

    Updated doc and readme

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 //!
 //! Client/Server application that allows you to broadcast data from clients to all the other
 //! clients connected to the server
+//!
+//! # Example:
+//!
+//! cargo run -- --server --host 127.0.0.1 --port 12345
+//!
+//! This command runs the app in the server mode listening on localhost port 12345
+//!
+//! cargo run -- --client --host 127.0.0.1 --port 12345
+//!
+//! This command runs the app in the client mode connecting to localhost on port 12345
+//!
+//! If you need to debug the app you can enable logs by changing the 'RUST_LOG' variable
+//!
+//! RUST_LOG=info cargo run -- --server --host 127.0.0.1 --port 12345
+//!
 
 use clap::{command, Parser, ArgGroup};
 use std::net::{TcpStream, TcpListener};
